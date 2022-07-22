e14abfc6eba9fb646a43bc95f55bd798028037f4
aeh1707d <abdelhadi.hireche@outlook.com>
2022-07-22 14:00:53 +0100

    Configure router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Categories from './components/Categories';
+import Books from './components/Books';
 
-function App() {
-  return (
-    <div className="App" />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Books />} />
+            <Route path="/Categories" element={<Categories />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
